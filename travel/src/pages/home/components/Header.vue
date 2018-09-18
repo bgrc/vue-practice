fccb25427c78d21c4321d5d67ceb8316df6773af
xnng <xnng77@gmail.com>
2018-09-18 17:43:46 +0800

    finished swiper

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="header">
-        <div class="header-left">
-            <div class="iconfont back-icon">&#xe645;</div>
-        </div>
-        <div class="header-input">
-            <span class="iconfont">&#xe632;</span>
-            输入城市/景点/游玩主题
-        </div>
-        <div class="header-right">
-            城市
-            <span class="iconfont arrow-icon">&#xe600;</span>
-        </div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe645;</div>
     </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,30 +23,30 @@ export default {
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 .header
-    display: flex
-    flex-direction: row
-    justify-content: space-between
-    height: 0.86rem
-    align-items: center
-    background: $bgcolor
-    color: #fff
-    .header-left
-        width: 0.77rem
-        .back-icon
-            text-align: center
-            font-size: 0.36rem
-    .header-input
-        flex: 1
-        color: #ccc
-        border-radius: 0.1rem
-        background: #fff
-        line-height: 0.6rem
-        padding-left: 0.2rem
-    .header-right
-        text-align: center
-        width: 1.24rem
-        margin-right: 0.1rem
-        .arrow-icon
-            font-size: 0.18rem
-            margin-left: -0.04rem
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  height: 0.86rem
+  align-items: center
+  background: $bgcolor
+  color: #fff
+  .header-left
+    width: 0.77rem
+    .back-icon
+      text-align: center
+      font-size: 0.36rem
+  .header-input
+    flex: 1
+    color: #ccc
+    border-radius: 0.1rem
+    background: #fff
+    line-height: 0.6rem
+    padding-left: 0.2rem
+  .header-right
+    text-align: center
+    width: 1.24rem
+    margin-right: 0.1rem
+    .arrow-icon
+      font-size: 0.18rem
+      margin-left: -0.04rem
 </style>
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true,
+        autoplay: {
+          delay: 3000
+        }
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/2d/0d7dc3400a50b502.jpg_750x200_4ca95dd4.jpg'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/d4/42de11c003f74302.jpg_750x200_cdab8b96.jpg'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/fe/ca8ed7adc3ff9f02.jpg_750x200_49735140.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  width: 100%
+  height: 31.25vw
+  background: #fff
+  .swiper-img
+    width: 100%
+</style>
